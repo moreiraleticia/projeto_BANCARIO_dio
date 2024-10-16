f83482c831995b184203fbe30eedb21d327c074e
leticiamoreira <leticia.cassia1@gmail.com>
2024-10-16 15:34:05 -0300

    criação de pacotes e de ambiente de desenvolvimento

---
+++ meu_pacote/README.md
@@ -0,0 +1,4 @@
+# projeto_BANCARIO_dio
+projeto desenvolvido, em Python, com parceria da plataforma educacional DIO
+Atualizações feita diariamente .
+A cada desafio proposto pela plataforma.
---
+++ meu_pacote/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='meu_pacote',
+    version='0.1',
+    packages=find_packages(),
+    description='Pacote criado para projeto bancario Dio',
+    author='Leticia Moreira',
+    author_email='leticia.cassia1@gmail.com',
+    url='https://github.com/moreiraleticia/projeto_BANCARIO_dio.git',
+)
